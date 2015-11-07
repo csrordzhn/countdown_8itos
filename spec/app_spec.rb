debfec88cb7116319b0097d20d071dbeaa3c19fb
Cesar Ordonez <c_man182@yahoo.com>
2015-11-07 14:31:02 +0800

    added first draft of specs for feature testing

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,5 +5,20 @@ require_relative '../app'
 include Rack::Test::Methods
 
 def app
-  
+  Info8itos
+end
+
+describe 'Getting the root of the service' do
+  it 'should return ok' do
+    get '/'
+    last_response.must_be :ok?
+  end
+end
+
+describe 'Using the countdown feature' do
+  it 'should return a string with a message containing the number of days' do
+    get '/countdown'
+    last_response.must_be :ok?
+    last_response.body.must_match(/[0-9]/)
+  end
 end
